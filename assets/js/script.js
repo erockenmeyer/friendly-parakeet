cbdbad17ef7bd3e19da0e5479b7d5dbe18d180fb
erockenmeyer <erikrockenmeyer@gmail.com>
2022-02-23 21:45:10 -0600

    added ability to ask for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 // Assignment code here
+// Possible characters as global variable strings
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var numerical = "1234567890";
 var special = "!#$%'()*+,-./:;<=>?@[]^_`{}|~";
+
+var checkLength = function() {
+  passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.")
+  passLength = parseInt(passLength);
+  if (passLength >= 8 && passLength <= 128) {
+    return passLength;
+  } else {
+    alert("You did not enter a valid number. Please try again.");
+    checkLength();
+  }
+}
+
+var generatePassword = function() {
+  var passLength = checkLength();
+  console.log(passLength);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
